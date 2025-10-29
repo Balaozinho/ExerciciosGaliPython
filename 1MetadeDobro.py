20b33f8b3bcef8ed6549f0a54b458dea0b143b03
Balaozinho <ogrupogali@gmail.com>
2025-10-28 22:33:13 -0300

    Primeiro Commit

---
+++ 1MetadeDobro.py
@@ -0,0 +1,10 @@
+#EX 1 - PEÇA PARA O USUÁRIO INFORMAR UM NÚMERO
+#MOSTRE QUAL É O DOBRO E A METADE DAQUELE NÚMERO
+
+num = float(input("Digite o número: "))
+
+metade = num / 2
+dobro = num * 2
+
+print("A metade do número é:", metade)
+print("O dobro do número é:", dobro)
