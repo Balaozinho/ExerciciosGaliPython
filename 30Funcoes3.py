4bc6ef54cf1768376fb99a8117236ba63f692c20
Balaozinho <ogrupogali@gmail.com>
2025-11-01 16:32:30 -0300

    Adicionamos o main

--- 30Funcoes3.py
+++ 30Funcoes3.py
@@ -6,6 +6,7 @@
 #CHARMANDER, VIDA: 100, ATAQUE: BOLA DE FOGO, DANO: 35
 #SQUIRTLE, VIDA: 100, ATAQUE: JATO DE ÁGUA, DANO: 32
 #BULBASSAURO, VIDA: 100, ATAQUE: CHICOTE DE PLANTA, DANO: 30
+#TENTE UTILIZAR A FUNCAO MAIN E __IF NAME__ == "__MAIN__"
 
 import random
 
@@ -34,36 +35,14 @@ def selecionar_pokemon(jogador) :
     
     return escolha
 
-escolha1 = selecionar_pokemon(jogador1)
-escolha2 = selecionar_pokemon(jogador2)
-
-print (f'O jogador1 escolhe {escolha1}')
-print (f'O jogador2 escolhe {escolha2}')
-
-
 def ficha_habilidades(jogador) :
     print("FICHA HABILIDADES".center(40,"*"))
     print(f'Vida = 100| Ataque = {pokemons[jogador]['Ataque']}| Dano = {pokemons[jogador]['Dano']}')
 
-print('-'*40) 
-ficha_habilidades(escolha1)
-ficha_habilidades(escolha2)
-
 def ataque(escolha):
     return pokemons[escolha]['Dano']*random.uniform(1,2)
 
-print('-'*40)
-dano1 = ataque(escolha1)
-dano2 = ataque(escolha2)
-
-print(f'O dano de {jogador1} foi de {pokemons[escolha1]['Dano']*random.uniform(1,2):.2f}')
-print(f'O dano de {jogador2} foi de {pokemons[escolha2]['Dano']*random.uniform(1,2):.2f}')
-
-
-ataque(escolha1)
-ataque(escolha2)
-
-def batalha():
+def batalha(escolha1, dano1, escolha2, dano2):
     vida1 = 100 - dano2
     vida2 = 100 - dano1
     if vida1 > vida2 :
@@ -71,5 +50,30 @@ def batalha():
     else:
         print(f'O vencedor foi {escolha2} pois {escolha1} ficou com menos vida: {vida1:.2f}')
 
-print("-"*40)
-batalha()
+
+def main ():
+    escolha1 = selecionar_pokemon(jogador1)
+    escolha2 = selecionar_pokemon(jogador2)
+
+    print (f'O jogador1 escolhe {escolha1}')
+    print (f'O jogador2 escolhe {escolha2}')
+
+    print('-'*40) 
+    ficha_habilidades(escolha1)
+    ficha_habilidades(escolha2)
+
+    print('-'*40)
+    dano1 = ataque(escolha1)
+    dano2 = ataque(escolha2)
+
+    print(f'O dano de {jogador1} foi de {pokemons[escolha1]['Dano']*random.uniform(1,2):.2f}')
+    print(f'O dano de {jogador2} foi de {pokemons[escolha2]['Dano']*random.uniform(1,2):.2f}')
+
+    ataque(escolha1)
+    ataque(escolha2)
+
+    print("-"*40)
+    batalha(escolha1,dano1,escolha2,dano2)
+
+if __name__ == '__main__':
+    main()
